8d1f7e388f80d39ccda0a4655bc9907b2e031858
BrianG22 <moso2201@gmail.com>
2025-11-09 14:34:18 -0700

    estados

--- react-youtube-2/src/App.jsx
+++ react-youtube-2/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const getInitialLikes = () => {
+  console.log('getInitialLikes');
+  return 12;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const Likes = () => {
+  console.log('Render Likes');
 
-export default App
+  const [likes, setLikes] = useState(getInitialLikes);
+
+  return <Button onClick={() => setLikes(likes + 1)}>{likes} likes</Button>;
+};
+
+const Button = ({ onClick, children }) => {
+  console.log('Render Button');
+  return <button onClick={onClick}>{children}</button>;
+};
+
+const App = () => {
+  console.log('Render App');
+  return <Likes />;
+};
+
+export default App;
